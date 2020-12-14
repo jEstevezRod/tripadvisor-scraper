b54039f5e834c4341990e32d0a8384c9c7832a82
Jesus Estevez <jesestrod1@gmail.com>
2020-12-14 16:24:06 +0100

    first commit

--- src/tools/general.js
+++ src/tools/general.js
@@ -65,6 +65,8 @@ async function resolveInBatches(promiseArray, batchLength = 10) {
 
 const processReview = (review, remoteId) => {
     const { text, title, rating, tripInfo, publishedDate, userProfile } = review;
+    log.debug(review);
+
     const stayDate = tripInfo ? tripInfo.stayDate : null;
     let userLocation = null;
     let userContributions = null;
